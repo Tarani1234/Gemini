b449ba019fe79485b01e20d1f79517d8d94ba1e8
Tarani1234 <maliyalatarani@gmail.com>
2024-03-15 19:16:18 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBar from './Components/SideBar/SideBar'
+import Main from './Components/SideBar/Main/Main'
+
+const App = () => {
+  return (
+    <>
+     <SideBar/>
+     <Main/>
+    </>
+  )
+}
+
+export default App
